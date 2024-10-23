1bdece43c98c930566a251aac8aacfaf25254a3e
GuiFV <guilhermeviotti@gmail.com>
2024-10-23 18:04:01 +0100

    changed view to manipulate order of top menu

--- guilsportfolio/core/views.py
+++ guilsportfolio/core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.utils.text import slugify
+from django.db.models import Case, When
 
-from guilsportfolio.core.models import Profile, Project, Extra
+from guilsportfolio.core.models import Profile, Project, Extra, Expertise
 
 
 def home(request):
 
     profile = Profile.objects.first()  # Only one Profile is allowed in the DB
     projects = Project.objects.filter(executive__name=profile.name)
+    expertises = Expertise.objects.filter(profile=profile).order_by(
+        Case(  # Implemented to manipulate the order of top menu
+            When(id=2, then=0),  # First, Software Developer
+            When(id=1, then=1),  # Then, Project Manager
+            default=2,  # Finally, the rest
+        ),
+        'expertise'
+    )
+
     display = None  # For filtering expertises and manipulating button's logic
     extras = Extra.objects.all()
 
@@ -17,7 +27,7 @@ def home(request):
         projects = projects.filter(areas_of_expertise__expertise__icontains=expertise)
 
     context = {
-        'profile': profile, 'projects': projects, 'display': display, 'extras': extras,
+        'profile': profile, 'projects': projects, 'display': display, 'extras': extras, 'expertises': expertises,
     }
 
     return render(request, 'index.html', context)
